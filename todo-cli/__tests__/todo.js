38d4ab8b3cd4cf19fad0c5d80078df056641fac3
vishwateja231 <vishwateja2345@gmail.com>
2023-12-18 14:45:10 +0530

    hello

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,69 +1,34 @@
-/* eslint-disable no-undef */
-const todoList = require("../todo");
+const { describe, beforeAll, test, expect } = require("@jest/globals");
+const { DataTypes } = require("sequelize");
+const db = require("../models/index");
 
-const { add, markAsComplete, all, dueToday, dueLater, overdue } = todoList();
+describe("Todolist Test Suite", () => {
+  beforeAll(async () => {
+    // Initialize the Todo model
+    await db.Todo.init(
+      {
+        title: DataTypes.STRING,
+        dueDate: DataTypes.DATEONLY,
+        completed: DataTypes.BOOLEAN,
+      },
+      {
+        sequelize: db.sequelize,
+        modelName: "Todo",
+      },
+    );
 
-describe("Todo test suite", () => {
-  test("should add new todo", () => {
-    expect(all.length).toBe(0);
+    // Sync the model with the database
+    await db.Todo.sync({ force: true });
+  });
 
-    const date = new Date();
-    const yd = new Date(date);
-    const td = new Date(date);
-
-    td.setDate(date.getDate() + 1);
-    yd.setDate(date.getDate() - 1);
-
-    add({
-      title: "Todo test",
+  test("Should add new todo", async () => {
+    const todoItemsCount = await db.Todo.count();
+    await db.Todo.addTask({
+      title: "Test todo",
       completed: false,
-      dueDate: new Date().toLocaleDateString("en-CA"),
+      dueDate: new Date(),
     });
-
-    add({
-      title: "Todo test",
-      completed: false,
-      dueDate: yd.toLocaleDateString("en-CA"),
-    });
-
-    add({
-      title: "Todo test",
-      completed: false,
-      dueDate: td.toLocaleDateString("en-CA"),
-    });
-
-    expect(all.length).toBe(3);
-  });
-
-  test("should mark a todo as complete", () => {
-    expect(all[0].completed).toBe(false);
-
-    markAsComplete(0);
-
-    expect(all[0].completed).toBe(true);
-  });
-
-  test("should retrieve a todo as due today", () => {
-    expect(all.length).toBe(3);
-
-    const k = dueToday();
-    console.log(k);
-    expect(k.length).toBe(1);
-  });
-
-  test("should retrieve a todo as overdue", () => {
-    let k = [];
-    expect(k.length).toBe(0);
-
-    k = overdue();
-    expect(k.length).toBe(1);
-  });
-
-  test("should retrieve a todo as due later", () => {
-    let k = [];
-    expect(k.length).toBe(0);
-
-    k = dueLater();
-    expect(k.length).toBe(1);
+    const newTodoItemsCount = await db.Todo.count();
+    expect(newTodoItemsCount).toBe(todoItemsCount + 1);
   });
 });
---
+++ todo-cli/gg/TodoModel.js
@@ -0,0 +1,32 @@
+const { DataTypes, Model } = require("sequelize");
+const { sequelize } = require("./connectDB.js");
+
+class Todo extends Model {
+  static async addTask(params) {
+    return await Todo.create(params);
+  }
+  displayablestring() {
+    return `${this.id} - ${this.title} (${this.dueDate}) - Completed: ${this.completed}`;
+  }
+}
+Todo.init(
+  {
+    // Model attributes are defined here
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    dueDate: {
+      type: DataTypes.DATEONLY,
+    },
+    completed: {
+      type: DataTypes.BOOLEAN,
+    },
+  },
+  {
+    sequelize,
+  },
+);
+
+Todo.sync();
+module.exports = { Todo }; // Explicitly naming the exported object
